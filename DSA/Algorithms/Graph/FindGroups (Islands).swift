d772458cbc77b58f3bd53b8157eae8e304fc9456
Muhammad Shayan <shayanhaider1999@gmail.com>
2022-11-21 13:17:37 +0500

    add find no. of groups (no. of islands) algorithm

---
+++ DSA/Algorithms/Graph/FindGroups (Islands).swift
@@ -0,0 +1,67 @@
+//
+//  FindGroups (Islands).swift
+//  DSA
+//
+//  Created by Muhammad Shayyan on 21/11/2022.
+//
+
+import Foundation
+
+/*
+ Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+ An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ 
+ let arr = [ [1, 1, 0, 0, 0],
+             [1, 1, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0],
+             [1, 1, 0, 0, 1]] There are 3 groups
+ */
+
+class NoOfGroupsFinder {
+    
+    func findGroups(arr: inout [[Int]]) -> Int {
+        var numberOfGroups = 0
+        
+        for i in 0..<arr.count {
+            for j in 0..<arr[i].count {
+                // count number of groups
+                numberOfGroups += traverseGraph(arr: &arr, row: i, col: j)
+            }
+        }
+            
+        return numberOfGroups
+    }
+
+    func traverseGraph(arr: inout [[Int]], row: Int, col: Int) -> Int {
+        if row >= arr.count {
+            return 0
+        }
+        if row < 0 || col < 0 {
+            return 0
+        }
+        if col >= arr[row].count {
+            return 0
+        }
+        if arr[row][col] == 0 {
+            return 0
+        }
+        
+        arr[row][col] = 0
+
+        // Verticals and Horizontals
+        traverseGraph(arr: &arr, row: row + 1, col: col)
+        traverseGraph(arr: &arr, row: row - 1, col: col)
+        traverseGraph(arr: &arr, row: row, col: col - 1)
+        traverseGraph(arr: &arr, row: row, col: col + 1)
+        
+        // Diagonals
+        traverseGraph(arr: &arr, row: row + 1, col: col + 1)
+        traverseGraph(arr: &arr, row: row - 1, col: col - 1)
+        traverseGraph(arr: &arr, row: row + 1, col: col - 1)
+        traverseGraph(arr: &arr, row: row - 1, col: col + 1)
+        
+        return 1
+    }
+}
---
+++ DSA/Data Structures/SinglyLinkedList.swift
@@ -0,0 +1,9 @@
+//
+//  SinglyLinkedList.swift
+//  DSA
+//
+//  Created by Muhammad Shayyan on 19/11/2022.
+//
+
+import Foundation
+
