4ceea714cc87cd89483c1f25c66c6f3ce259450a
Muhammad Shayan <shayanhaider1999@gmail.com>
2022-12-06 10:15:26 +0500

    implement connected components count algorithm for adjacency dictionary.

--- DSA/Algorithms/Graph/FindGroups (Islands).swift
+++ DSA/Algorithms/Graph/FindGroups (Islands).swift
@@ -9,19 +9,19 @@ import Foundation
 
 /*
  Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
-
+ 
  An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
  
  let arr = [ [1, 1, 0, 0, 0],
-             [1, 1, 0, 0, 0],
-             [0, 0, 0, 0, 0],
-             [0, 0, 0, 0, 0],
-             [1, 1, 0, 0, 1]] There are 3 groups
+ [1, 1, 0, 0, 0],
+ [0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0],
+ [1, 1, 0, 0, 1]] There are 3 groups
  
  ref: https://leetcode.com/problems/number-of-islands/
  */
 
-class NoOfGroupsFinder {
+class NoOfGroupsFinderFromArray {
     
     func findGroups(arr: inout [[Int]]) -> Int {
         var numberOfGroups = 0
@@ -32,10 +32,10 @@ class NoOfGroupsFinder {
                 numberOfGroups += traverseGraph(arr: &arr, row: i, col: j)
             }
         }
-            
+        
         return numberOfGroups
     }
-
+    
     func traverseGraph(arr: inout [[Int]], row: Int, col: Int) -> Int {
         if row >= arr.count {
             return 0
@@ -51,7 +51,7 @@ class NoOfGroupsFinder {
         }
         
         arr[row][col] = 0
-
+        
         // Verticals and Horizontals
         traverseGraph(arr: &arr, row: row + 1, col: col)
         traverseGraph(arr: &arr, row: row - 1, col: col)
@@ -65,5 +65,34 @@ class NoOfGroupsFinder {
         traverseGraph(arr: &arr, row: row - 1, col: col + 1)
         
         return 1
+    }
+}
+
+class NoOfGroupsFinderFromAdjacencyDictionary {
+    
+    func findNoOfConnectedGroups(graph: [String: [String]]) -> Int {
+        var visited = Set<String>()
+        
+        var count = 0
+        for key in graph.keys {
+            if explore(graph: graph, node: key, visited: &visited) {
+                count += 1
+            }
+        }
+        
+        return count
+    }
+    
+    func explore(graph: [String: [String]], node: String, visited: inout Set<String>) -> Bool {
+        if visited.contains(node) {
+            return false
+        }
+        visited.insert(node)
+        
+        for neighbour in graph[node]! {
+            explore(graph: graph, node: neighbour, visited: &visited)
+        }
+        
+        return true
     }
 }
